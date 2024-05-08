c927e8e7182c9e3f2e0d257eb4af8160f1be1f83
Mafioza77 <160590230+Mafioza77@users.noreply.github.com>
2024-05-08 18:27:56 +0200

    Update script.js

--- StronaZaliczeniowa/script.js
+++ StronaZaliczeniowa/script.js
@@ -1,17 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.image img');
     const overlay = document.getElementById('overlay');
     const overlayImage = document.getElementById('overlay-image');
     const closeBtn = document.getElementById('close-btn');
-    const galleryImages = document.querySelectorAll('.gallery-image');
 
-    galleryImages.forEach(image => {
-        image.addEventListener('click', function () {
-            overlayImage.src = this.src;
+    images.forEach(image => {
+        image.addEventListener('click', function() {
+            const src = this.getAttribute('src');
+            overlayImage.setAttribute('src', src);
             overlay.style.display = 'flex';
         });
     });
 
-    closeBtn.addEventListener('click', function () {
+    closeBtn.addEventListener('click', function() {
         overlay.style.display = 'none';
     });
 });
